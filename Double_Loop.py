dbd44727e913a81ff5288bbab1383925c8b65cac
Shaurya Verma <vshaurya2000@gmail.com>
2025-04-15 19:39:28 +1000

    Re configured Double_Loop.py

--- Double_Loop.py
+++ Double_Loop.py
@@ -1,33 +1,33 @@
-# for row in range(5):
-#    for col in range(5):
-#        print("a", end=" ")
-#    print("\n")  
+print("Printing 5 X 5 grid *")
+for row in range(5):
+   for col in range(5):
+       print("*", end=" ")
+   print("\n")  
     
+
 # let num = 1
 # then num+1 per tick in the loop 
 
-# num = 1
-# for row in range(5):
-#     for col in range(5):
-#         print(num, end=" ")
-#         num += 1
-#     print("\n")
-  
-for r in range(5):
-    for c in range(5):
-        print((r,c), end=" ")
-        
+print("Printing numbered 5 X 5 grid")
+num = 1
+for row in range(5):
+    for col in range(5):
+        print(num, end=" ")
+        num += 1
     print("\n")
 
+print("Printing co-ordinate positions of each place in the 5 X 5 grid")
+for row in range(1,6):
+   for col in range(row):
+       print("a", end=" ")
+   print("\n") 
 
-# for row in range(1,6):
-#    for col in range(row):
-#        print("a", end=" ")
-#    print("\n") 
-
-for row in range():
-   for col in range():
+print("Printing a right angle triangle with character a")
+for r in range():
+   for c in range():
        print("a", end=" ")
    print("\n")
+
+
 
 
