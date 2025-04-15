9c5df1fbbac6735ac2eb82da2799c7df9aa36c94
Shaurya Verma <vshaurya2000@gmail.com>
2025-04-15 19:51:26 +1000

    Finished Double_Dimension_Array

--- Double_Dimension_Array.py
+++ Double_Dimension_Array.py
@@ -1,8 +1,14 @@
 grid = []
 num = 1
+
 for row in range(5):
-    new_row = []
+    new_row = []  
     for col in range(5):
-        new_row.append
-        grid.append(new_row)
+        new_row.append(num)
         num += 1
+    grid.append(new_row)
+
+for row in grid:
+    for i in row:
+        print(i, end=" ")
+    print("\n")
