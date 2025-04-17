e4e304df40221dfa5494dfd9ec3d8831d23812f0
Shaurya Verma <vshaurya2000@gmail.com>
2025-04-17 13:59:34 +1000

    Just fixing things up

--- Functions & Recursion.py
+++ Functions & Recursion.py
@@ -1,39 +1,42 @@
-#def calculation (a,b,c):
-#    sum = a + b + c
-#    average = sum/3
-#    print(average)
+def calculation (a,b,c):
+   sum = a + b + c
+   average = sum/3
+   return average
 
-#calculation(60,50,54)
-
-#print("Shaurya", end = " ")
-#print("Verma")
-#states = ["Bihar","Maharastra", "Punjab","Tamil Nadu","Kashmir"]
-#players = ["Kohli","Bumrah","Smith","Warner","Sandpaper","Shreyas"]
-#def size_list (list):
-#    print(len(list))
-
-#size_list(states)
-#size_list(players)
-
-#countries = ["Australia","India","Indonesia","Bhutan","Nepal"]
-
-#def print_countries(countries):
-#    for item in countries:
-#        print(item, end=" ")
+avr = calculation(60,50,54)
+print('Average: ',avr)
 
 
-#print_countries(countries)
-#print()
+
+print("Shaurya", end = " ")
+print("Verma")
+states = ["Bihar","Maharastra", "Punjab","Tamil Nadu","Kashmir"]
+players = ["Kohli","Bumrah","Smith","Warner","Sandpaper","Shreyas"]
+def size_list (list):
+   print(len(list))
+
+size_list(states)
+size_list(players)
+
+countries = ["Australia","India","Indonesia","Bhutan","Nepal"]
+
+def print_countries(countries):
+   for item in countries:
+       print(item, end=" ")
 
 
-#def add_numbers(a,b):
-#    return a + b
+print_countries(countries)
+print()
 
-#a = int(input("Enter the first number: "))
-#b = int(input("Enter the second number: "))
 
-#c =  add_numbers(a,b)
-#print(c)
+def add_numbers(a,b):
+   return a + b
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+c =  add_numbers(a,b)
+print(c)
 
 #RECURSIONS 
 def show(n):
---
+++ Maths.py
@@ -0,0 +1,8 @@
+
+num = int(input("Put a number in:"))
+
+total = 0
+for i in range(num,0,-1):
+    total += i
+
+print(total)
