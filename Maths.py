0de76860233e9bc0cf0bfd625eb997137bf0c64f
Shaurya Verma <vshaurya2000@gmail.com>
2025-04-17 14:12:56 +1000

    Expanding and fixing the functions

--- Maths.py
+++ Maths.py
@@ -1,8 +1,16 @@
+def sum_down_from(num):
+    total = 0
+    for i in range(num, 0, -1):
+        total += i
+    return total
 
-num = int(input("Put a number in:"))
+# num = int(input("Put a number in: "))< ------- The way to print the funtion
+# result = sum_down_from(num)
+# print(result)
 
-total = 0
-for i in range(num,0,-1):
-    total += i
-
-print(total)
+def add_numbers(a,b):
+   return a + b
+# a = int(input("Enter the first number: "))    < ----- The way to print the funtion
+# b = int(input("Enter the second number: "))
+# c =  add_numbers(a,b)
+# print(c)
